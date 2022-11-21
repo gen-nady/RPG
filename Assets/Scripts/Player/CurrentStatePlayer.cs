689bc40d0fce9e42f278e37be93ea8acbbbc4268
Gennadiy Shushkevich <gennadiy13122@gmail.com>
2022-11-21 17:53:24 +0300

    add-main-player

---
+++ Assets/Scripts/Player/CurrentStatePlayer.cs
@@ -0,0 +1,9 @@
+﻿namespace Player
+{
+    public enum CurrentStatePlayer
+    {
+        idle,
+        run,
+        jump
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private FixedJoystick _fixedJoystick;
+        [SerializeField] private float _speed;
+        private Animator _animator;
+        private Rigidbody _rigidbody;
+        private CurrentStatePlayer _currentStatePlayer;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _animator = GetComponent<Animator>();
+            _currentStatePlayer = CurrentStatePlayer.idle;
+        }
+
+        private void FixedUpdate()
+        {
+            _rigidbody.velocity = new Vector3(_fixedJoystick.Direction.x, 0, _fixedJoystick.Direction.y) * _speed * Time.fixedDeltaTime;
+            if ((_fixedJoystick.Direction.x > 0 || _fixedJoystick.Direction.y > 0) && _currentStatePlayer != CurrentStatePlayer.run)
+            {
+                _animator.SetTrigger("Run");
+                _currentStatePlayer = CurrentStatePlayer.run;
+            }
+            if((_fixedJoystick.Direction.x == 0 && _fixedJoystick.Direction.y == 0) &&_currentStatePlayer != CurrentStatePlayer.idle)
+            {
+                _animator.SetTrigger("Idle");
+                _currentStatePlayer = CurrentStatePlayer.idle;
+            }
+        }
+    }
+}
