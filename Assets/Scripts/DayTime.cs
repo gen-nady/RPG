bc454c374d37bc2de389f0085ab362601ca3fdf9
Gennadiy Shushkevich <gennadiy13122@gmail.com>
2022-11-27 19:46:48 +0300

    Merge

--- Assets/Scripts/DayTime.cs
+++ Assets/Scripts/DayTime.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
-using Random = UnityEngine.Random;
+﻿using UnityEngine;
 
 [ExecuteInEditMode]
 public class DayTime : MonoBehaviour
